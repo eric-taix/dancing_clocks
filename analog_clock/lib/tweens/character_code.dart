e112ffd926ca97752f707a29fd6465e52d641801
Eric Taix <eric.taix@teads.tv>
2019-12-28 22:22:25 +0100

    Encode first characters

--- analog_clock/lib/tweens/character_code.dart
+++ analog_clock/lib/tweens/character_code.dart
@@ -4,6 +4,32 @@ const double pi = 3.1415926535897932;
 
 const _space = 625.625;
 
+
+/// A character is defined by
+/// - a width (number of clocks in the horizontal axis)
+/// - an array of double
+///
+/// Each double encodes two angles (hours and minutes) and is expressed in thousandths of 2*PI:
+/// the integer part for the first angle and the fractional part for the second angle.
+///
+/// The angles are computed in radian and start from the vertical top (0) and increase clockwise to 1000 (2 * PI)
+///
+/// Decimal cheat sheet:
+/// 0:   top
+/// 125: top right
+/// 250: right
+/// 375: bottom right
+/// 500: bottom
+/// 625: bottom left
+/// 750: left
+/// 875: top left
+///
+/// Note: you don't have, of course, to use these predefined values and define specific angles
+///
+/// Examples:
+/// 500.250 defines hours at bottom and minutes at right (i.e. PI for hours and PI/2 for minutes)
+/// 875.625 defines hours at top left and minutes at bottom left (i.e. 7*PI/8 for hours and 5*PI/4 for minutes)
+///
 void registerCharacters() {
   CharacterCode.registerChar(
       "0",
@@ -45,10 +71,10 @@ class CharacterCode {
 
   CharacterCode(this.width, List<double> definition)
       : angles = definition.map((value) {
-          var i = value.floor();
-          var d = (value - i) * 1000;
-          return ClockAngles(i * math.pi / 1000, d * math.pi / 1000);
-        }).toList();
+    var i = value.floor();
+    var d = (value - i) * 1000;
+    return ClockAngles(i * math.pi / 1000, d * math.pi / 1000);
+  }).toList();
 
   static void registerChar(String char, CharacterCode code) => _chars[char] = code;
 
